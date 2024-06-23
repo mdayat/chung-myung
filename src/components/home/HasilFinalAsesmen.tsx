2902abb408b6192c4d9781ce5802643ab8c4b0eb
Wahyudi Umar <wahyucherryorlove@gmail.com>
2024-06-24 00:06:50 +0700

    feat: add hasil final asesmen component

---
+++ src/components/home/HasilFinalAsesmen.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@components/Button";
+import { Typography } from "@components/Typography";
+
+import { ChevronRight } from "@components/icons/ChevronRight";
+import { School } from "@components/icons/School";
+
+export function HasilFinal() {
+  return (
+    <div className="bg-neutral-0 shadow-lg border-t-2 border-neutral-50 flex p-5 rounded-xl relative">
+      <div className="absolute w-20 h-20 block bg-secondary-100/80 top-0 left-0 rounded-full blur" />
+      <div className="relative">
+        <School className="w-8 h-8 fill-secondary-500" />
+      </div>
+
+      <div className="flex py-2 ml-10 items-center justify-between w-full">
+        <div className="max-w-[293px] mr-10">
+          <Typography variant="h5" weight="bold" className="mb-1.5">
+            Hasil Final Asesmen
+          </Typography>
+          <Typography variant="p3" className="text-neutral-400">
+            Lihat kembali bukti dari perjuanganmu belajar di Emteka.
+          </Typography>
+        </div>
+
+        <Button size="medium" disabled>
+          Lihat hasil <ChevronRight className="w-7 h-7" />
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
 import { Navbar } from "@components/Navbar";
 
-import { Jumbotron } from "@components/home/Jumbotron";
 import { AsesmenStart } from "@components/home/MulaiAsesmen";
+import { HasilFinal } from "@components/home/HasilFinalAsesmen";
+import { HasilAsesmen } from "@components/home/HasilAsesmen";
+import { Jumbotron } from "@components/home/Jumbotron";
 
 export default function Home() {
   return (
     <>
       <Navbar />
 
-      <main>
+      <main className="mb-20">
         <Jumbotron />
 
         <AsesmenStart />
+
+        <ul className="mt-8 mx-20 grid grid-cols-2 gap-x-8 items-center">
+          <li>
+            <HasilAsesmen />
+          </li>
+          <li>
+            <HasilFinal />
+          </li>
+        </ul>
       </main>
     </>
   );

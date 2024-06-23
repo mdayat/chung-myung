884a704957a0df6bed0fbf26fbdc5ea0592bfd61
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-06-23 21:18:03 +0700

    feat: akb functionality and ui

--- src/pages/akb/index.tsx
+++ src/pages/akb/index.tsx
@@ -1,17 +1,189 @@
 import { useEffect, useState } from "react";
-import { AKBSample } from "src/data/AKBSample";
-import dataJson from "src/data/AKBSample.json";
+import { type AKBSample, type question } from "../../data/AKBSample";
+import EmtekaLogo from "../../../public/Emteka-Logo.svg";
+import Timer from "../../../public/timer.svg";
+import dataJson from "../../data/AKBSample.json";
+import Image from "next/image";
+import { RadioGroup, RadioGroupItem } from "@components/RadioButton";
+import { Label } from "@components/Label";
+import { Button } from "@components/Button";
 
 export default function AKB() {
   const [data, setData] = useState<AKBSample[]>([]);
+  const [selectedData, setSelectedData] = useState<AKBSample | null>(null);
+  const [selectedQuestion, setSelectedQuestion] = useState<question | null>(
+    null
+  );
+  const [timer, setTimer] = useState(0);
+  const [questionPage, setQuestionPage] = useState(1);
+  const [dataPage, setDataPage] = useState(1);
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
+  const [atRestRoom, setAtRestRoom] = useState(false);
+  function addLeadingZero(num: number, size: number) {
+    let s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
+  }
   useEffect(() => {
-    setData(dataJson);
+    const dataSorted = dataJson.sort(
+      (a, b) => a.sequence_number - b.sequence_number
+    );
+    setData(dataSorted);
+    setSelectedData(dataSorted[dataPage - 1]);
+    setSelectedQuestion(dataSorted[dataPage - 1].questions[questionPage - 1]);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].intersectionRatio === 1) {
+          setIsAnswerVisible(true);
+        } else {
+          setIsAnswerVisible(false);
+        }
+      },
+      { threshold: 0.25 }
+    );
+    const frameJawaban = document.getElementById("frame-jawaban");
+    if (frameJawaban != null) {
+      observer.observe(frameJawaban);
+    }
+    return () => observer.disconnect();
   }, []);
+  useEffect(() => {
+    if (selectedData == null) return;
+    setSelectedQuestion(selectedData?.questions[questionPage - 1]);
+  }, [questionPage]);
+  useEffect(() => {
+    if (selectedData == null) return;
+    setAtRestRoom(false);
+    setSelectedData(data[dataPage - 1]);
+    setQuestionPage(1);
+    setTimer(15);
+  }, [dataPage]);
+  useEffect(() => {
+    if (timer === 0) {
+      console.log("Time's up");
+      return;
+    }
+    const interval = setInterval(() => {
+      console.log(timer);
+      setTimer((prev: number) => prev - 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timer]);
   return (
     <div>
-      <h1 className="text-4xl font-bold">
-        <p>{data[0]?.name}</p>
-      </h1>
+      <div className="p-4 flex flex-row items-center justify-between">
+        <Image src={EmtekaLogo} alt="" className="h-8 max-w-fit" />
+        <div className="w-8 h-8 bg-neutral-500 rounded-full"></div>
+      </div>
+      {!atRestRoom && (
+        <div className="px-20 py-10">
+          <div className="flex flex-row items-center justify-between">
+            <p className="text-[2rem] font-bold">{selectedData?.name}</p>
+            <p className="text-base font-normal rounded-full border-[1px] border-neutral-950 px-4 py-2">
+              <span className="font-bold">{dataPage}</span>/{data.length}{" "}
+              <span className="font-bold">Subtes</span>
+            </p>
+          </div>
+          <div className="mt-4 p-4 bg-secondary-50 rounded-[8px] flex flex-row items-center justify-between">
+            <div className="flex flex-row">
+              {selectedData?.questions?.map((question, index) => (
+                <p
+                  key={question.id}
+                  className={`text-center w-10 h-10 rounded-full font-medium flex items-center justify-center ml-2 ${index + 1 === questionPage ? "bg-secondary-500 text-neutral-0" : "bg-neutral-0"}`}
+                  onClick={() => setQuestionPage(index + 1)}
+                >
+                  {addLeadingZero(index + 1, 2)}
+                </p>
+              ))}
+            </div>
+            <div className="flex flex-row items-center">
+              <Image src={Timer} alt="" className="h-6 w-6 mr-4" />
+              <div className="font-medium text-lg flex flex-row justify-center items-center">
+                <p className="bg-secondary-200 w-10 h-10 rounded-md flex items-center justify-center mr-2">
+                  {addLeadingZero(Math.floor(timer / 3600), 2)}
+                </p>
+                <p className="mr-2">:</p>
+                <p className="bg-secondary-200 w-10 h-10 rounded-md flex items-center justify-center mr-2">
+                  {addLeadingZero(Math.floor((timer / 60) % 60), 2)}
+                </p>
+                <p className="mr-2">:</p>
+                <p className="bg-secondary-200 w-10 h-10 rounded-md flex items-center justify-center">
+                  {addLeadingZero(Math.floor(timer % 60), 2)}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 flex flex-row">
+            <div className="w-[calc(100%-35%)]">
+              <p className="text-xl font-base">{selectedQuestion?.content}</p>
+            </div>
+            <div
+              id="frame-jawaban"
+              className="w-[calc(100%-65%)] flex flex-col justify-between"
+            >
+              <RadioGroup
+                defaultValue={selectedQuestion?.id_jawaban_user ?? ""}
+              >
+                {selectedQuestion?.multiple_choices.map((choice, index) => (
+                  <div
+                    key={index}
+                    className="bg-secondary-50 rounded-[8px] px-4 py-2 mb-4"
+                  >
+                    <div className="flex items-center space-x-2 my-2">
+                      <RadioGroupItem value={choice.id} id={choice.id} />
+                      <Label
+                        className="font-normal text-base"
+                        htmlFor={choice.id}
+                      >
+                        {choice.content}
+                      </Label>
+                    </div>
+                  </div>
+                ))}
+              </RadioGroup>
+              <div
+                key={questionPage}
+                className={`${isAnswerVisible ? "absolute bottom-8 translate-x-1/2" : "flex flex-row items-center justify-center"}`}
+              >
+                <Button
+                  variant={"secondary"}
+                  className="text-neutral-300 border-neutral-300 mr-3"
+                  onClick={() => {
+                    if (questionPage > 1) setQuestionPage((prev) => prev - 1);
+                  }}
+                >
+                  Kembali
+                </Button>
+                <Button
+                  className="ml-3"
+                  onClick={() => {
+                    if (questionPage < selectedData?.questions?.length! ?? 0)
+                      setQuestionPage((prev) => prev + 1);
+                    if (questionPage === selectedData?.questions?.length!) {
+                      setAtRestRoom(true);
+                    }
+                  }}
+                >
+                  {questionPage === selectedData?.questions?.length!
+                    ? "Subtes Selanjutnya"
+                    : "Selanjutnya"}
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {atRestRoom && (
+        <Button
+          className="ml-3"
+          onClick={() => {
+            if (dataPage === data.length) return;
+            setDataPage((prev) => prev + 1);
+          }}
+        >
+          {dataPage === data.length ? "Selesai" : "Subtes Selanjutnya"}
+        </Button>
+      )}
     </div>
   );
 }

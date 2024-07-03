4eaec10796f3d21a2c07f9ae31cf373b94652076
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-07-04 00:06:48 +0700

    feat: make dialog content conditional based on its type

--- src/components/shadcn/Dialog.tsx
+++ src/components/shadcn/Dialog.tsx
@@ -39,28 +39,46 @@ const DialogOverlay = forwardRef<
 ));
 DialogOverlay.displayName = Overlay.displayName;
 
+interface DialogContentProps extends ComponentPropsWithoutRef<typeof Content> {
+  type?: "lightbox" | "information";
+}
+
 const DialogContent = forwardRef<
   ElementRef<typeof Content>,
-  ComponentPropsWithoutRef<typeof Content>
->(({ className, children, ...props }, ref) => (
-  <DialogPortal>
-    <DialogOverlay />
-    <Content
-      ref={ref}
-      className={cn(
-        "fixed left-[50%] top-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-50%] bg-neutral-0 p-8 rounded-3xl shadow-[0_9px_18px_0_rgba(0,0,0,0.2)] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-75 data-[state=open]:zoom-in-75 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
-        className
-      )}
-      {...props}
-    >
-      {children}
-      <Close className="absolute right-4 top-4 rounded-full disabled:pointer-events-none">
-        <CloseIcon className="fill-neutral-800 w-6 h-6" />
-        <span className="sr-only">Close</span>
-      </Close>
-    </Content>
-  </DialogPortal>
-));
+  DialogContentProps
+>(({ className, children, type = "information", ...props }, ref) => {
+  const dialogContentBg =
+    type === "information" ? "bg-neutral-0" : "bg-neutral-100";
+
+  return (
+    <DialogPortal>
+      <DialogOverlay />
+      <Content
+        ref={ref}
+        className={cn(
+          "fixed left-1/2 top-1/2 z-50 w-full max-w-lg -translate-x-1/2 -translate-y-1/2 p-8 rounded-3xl shadow-[0_9px_18px_0_rgba(0,0,0,0.2)] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-75 data-[state=open]:zoom-in-75 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+          dialogContentBg,
+          className
+        )}
+        {...props}
+      >
+        {children}
+
+        {type === "information" ? (
+          <Close className="absolute right-4 top-4 rounded-full disabled:pointer-events-none">
+            <CloseIcon className="fill-neutral-800 w-6 h-6" />
+            <span className="sr-only">Close</span>
+          </Close>
+        ) : (
+          <Close className="absolute -top-16 left-1/2 -translate-x-1/2 border border-neutral-0 p-2 rounded-full disabled:pointer-events-none">
+            <CloseIcon className="fill-neutral-0 w-6 h-6" />
+            <span className="sr-only">Close</span>
+          </Close>
+        )}
+      </Content>
+    </DialogPortal>
+  );
+});
 DialogContent.displayName = Content.displayName;
 
 const DialogHeader = ({

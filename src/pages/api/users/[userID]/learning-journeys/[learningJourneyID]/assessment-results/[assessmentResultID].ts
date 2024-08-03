f7dad67dfb13266bd2c599c28cc4db88a120592f
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-08-04 01:43:39 +0700

    fix: update api handler to get and create assessment result

--- src/pages/api/users/[userID]/learning-journeys/[learningJourneyID]/assessment-results/[assessmentResultID].ts
+++ src/pages/api/users/[userID]/learning-journeys/[learningJourneyID]/assessment-results/[assessmentResultID].ts
@@ -9,41 +9,128 @@ export default async function handler(
   res: NextApiResponse<SuccessResponse | FailedResponse>,
 ) {
   res.setHeader("Content-Type", "application/json");
-  const userID = (req.query.learningJourneyID ?? "") as string;
+
+  // Check if "userID" is a valid UUID
+  const userID = (req.query.userID ?? "") as string;
+  let parseResult = zod.string().uuid().safeParse(userID);
+  if (parseResult.success === false) {
+    console.error(
+      new Error(`"userID" is not a valid UUID: `, { cause: parseResult.error }),
+    );
+
+    res.status(404).json({ status: "failed", message: "User Not Found" });
+    return;
+  }
+
+  // Check if "learningJourneyID" is a valid UUID
   const learningJourneyID = (req.query.learningJourneyID ?? "") as string;
-  const assessmentResultID = (req.query.assessmentResultID ?? "") as string;
+  parseResult = zod.string().uuid().safeParse(learningJourneyID);
+  if (parseResult.success === false) {
+    console.error(
+      new Error(`"learningJourneyID" is not a valid UUID: `, {
+        cause: parseResult.error,
+      }),
+    );
 
-  const uuidSchema = zod.object({
-    userID: zod.string().uuid(),
-    learningJourneyID: zod.string().uuid(),
-    assessmentResultID: zod.string().uuid(),
-  });
+    res
+      .status(404)
+      .json({ status: "failed", message: "Learning Journey Not Found" });
+    return;
+  }
 
-  const result = uuidSchema.safeParse({
-    userID,
-    learningJourneyID,
-    assessmentResultID,
-  });
-  if (result.success === false) {
-    res.status(400).json({
-      message: "Invalid User ID and Learning Journey ID",
-      status: "failed",
-    });
-    console.error("Invalid User ID and Learning Journey ID: ", result.error);
+  // Check if "userID" and "learningJourneyID" exist
+  try {
+    const results = await Promise.all([
+      supabase.from("user").select("id").eq("id", userID).maybeSingle(),
+      supabase
+        .from("learning_journey")
+        .select("id")
+        .eq("id", learningJourneyID)
+        .maybeSingle(),
+    ]);
+
+    if (results[0].error !== null) {
+      throw new Error(`Error when get a user based on "userID": `, {
+        cause: results[0].error,
+      });
+    }
+
+    if (results[1].error !== null) {
+      throw new Error(
+        `Error when get a learning journey based on "learningJourneyID": `,
+        { cause: results[1].error },
+      );
+    }
+
+    if (results[0].data === null) {
+      console.error(new Error(`User with "userID" is not found`));
+      res.status(404).json({ status: "failed", message: "User Not Found" });
+      return;
+    }
+
+    if (results[1].data === null) {
+      console.error(
+        new Error(`Learning Journey with "learningJourneyID" is not found`),
+      );
+
+      res
+        .status(404)
+        .json({ status: "failed", message: "Learning Journey Not Found" });
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "failed", message: "Server Error" });
     return;
   }
 
   if (req.method === "DELETE") {
+    // Check if "assessmentResultID" is a valid UUID
+    const assessmentResultID = (req.query.assessmentResultID ?? "") as string;
+    parseResult = zod.string().uuid().safeParse(assessmentResultID);
+    if (parseResult.success === false) {
+      console.error(
+        new Error(`"assessmentResultID" is not a valid UUID: `, {
+          cause: parseResult.error,
+        }),
+      );
+
+      res
+        .status(404)
+        .json({ status: "failed", message: "Assessment Result Not Found" });
+      return;
+    }
+
     try {
-      await supabase
+      const { data } = await supabase
         .from("assessment_result")
         .delete()
         .eq("id", assessmentResultID)
+        .select("id")
+        .maybeSingle()
         .throwOnError();
 
+      if (data === null) {
+        console.error(
+          new Error(`Assessment Result with "assessmentResultID" is not found`),
+        );
+
+        res
+          .status(404)
+          .json({ status: "failed", message: "Assessment Result Not Found" });
+        return;
+      }
+
       res.status(200).json({ status: "success", data: null });
-    } catch (err) {
-      res.status(500).json({ status: "failed", message: "error" });
+    } catch (error) {
+      console.error(
+        new Error(
+          `Error when delete an assessment result based on "assessmentResultID"`,
+          { cause: error },
+        ),
+      );
+
+      res.status(500).json({ status: "failed", message: "Server Error" });
     }
   } else {
     handleInvalidMethod(res, ["DELETE"]);

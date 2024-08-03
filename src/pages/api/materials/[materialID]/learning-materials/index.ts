74034c9e5132d3aa24e1ca742e0cbfcf5cc33c1c
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-08-03 12:59:12 +0700

    fix: update api handler to get learning material

--- src/pages/api/materials/[materialID]/learning-materials/index.ts
+++ src/pages/api/materials/[materialID]/learning-materials/index.ts
@@ -1,31 +1,40 @@
 import type { FailedResponse, SuccessResponse } from "@customTypes/api";
-import type { Enums as DBEnums } from "@customTypes/database";
+import type { LearningMaterial } from "@customTypes/learningMaterial";
 import { supabase } from "@lib/supabase";
 import { handleInvalidMethod } from "@utils/middlewares";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z as zod } from "zod";
-
-interface LearningMaterial {
-  id: string;
-  name: string;
-  description: string;
-  learningModuleURL: string;
-  type: DBEnums<"learning_material_type">;
-  sequenceNumber: number;
-}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<SuccessResponse<LearningMaterial[]> | FailedResponse>,
 ) {
   res.setHeader("Content-Type", "application/json");
-  const materialID = (req.query.materialID ?? "") as string;
 
-  const uuidSchema = zod.string().uuid();
-  const result = uuidSchema.safeParse(materialID);
-  if (result.success === false) {
-    console.error(new Error("Invalid Material ID: ", { cause: result.error }));
-    res.status(400).json({ status: "failed", message: "Invalid Material ID" });
+  // Check if "materialID" is a valid UUID
+  const materialID = (req.query.materialID ?? "") as string;
+  const parseResult = zod.string().uuid().safeParse(materialID);
+  if (parseResult.success === false) {
+    console.error(
+      new Error(`"materialID" is not a valid UUID: `, {
+        cause: parseResult.error,
+      }),
+    );
+
+    res.status(404).json({ status: "failed", message: "Material Not Found" });
+    return;
+  }
+
+  // Check if "materialID" exists
+  try {
+    if ((await isMaterialExist(materialID)) === false) {
+      console.error(new Error(`Material with "materialID" is not found`));
+      res.status(404).json({ status: "failed", message: "Material Not Found" });
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "failed", message: "Server Error" });
     return;
   }
 
@@ -34,7 +43,7 @@ export default async function handler(
       const { data } = await supabase
         .from("material_learning_material")
         .select(
-          "type:learning_material_type, sequenceNumber:learning_material_number, learning_material(id, name, description, learningModuleURL:learning_module_url)",
+          "type:learning_material_type, number:learning_material_number, learning_material(id, name, description, learningModuleURL:learning_module_url)",
         )
         .eq("material_id", materialID)
         .throwOnError();
@@ -44,23 +53,36 @@ export default async function handler(
         data: data!.map((learningMaterial) => ({
           ...learningMaterial.learning_material!,
           type: learningMaterial.type,
-          sequenceNumber: learningMaterial.sequenceNumber,
+          number: learningMaterial.number,
         })),
       });
     } catch (error) {
       console.error(
-        new Error(
-          "Invalid when get all learning material based on material ID: ",
-          { cause: error },
-        ),
+        new Error(`Error when get learning materials based on "materialID": `, {
+          cause: error,
+        }),
       );
 
-      res.status(500).json({
-        status: "failed",
-        message: "Server Error",
-      });
+      res.status(500).json({ status: "failed", message: "Server Error" });
     }
   } else {
     handleInvalidMethod(res, ["GET"]);
+  }
+}
+
+async function isMaterialExist(materialID: string): Promise<boolean> {
+  try {
+    const { data } = await supabase
+      .from("material")
+      .select("id")
+      .eq("id", materialID)
+      .maybeSingle()
+      .throwOnError();
+
+    return data !== null;
+  } catch (error) {
+    throw new Error(`Error when get a material based on "materialID": `, {
+      cause: error,
+    });
   }
 }

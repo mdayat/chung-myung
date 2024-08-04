87a35d0c3d9f66bc1118a631f5aa574ec1b84492
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-08-04 15:37:23 +0700

    feat: create post request handler to add studied learning material

--- src/pages/api/users/[userID]/learning-journeys/[learningJourneyID]/index.ts
+++ src/pages/api/users/[userID]/learning-journeys/[learningJourneyID]/index.ts
@@ -7,7 +7,9 @@ import { z as zod } from "zod";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<SuccessResponse<LearningJourney> | FailedResponse>,
+  res: NextApiResponse<
+    SuccessResponse<LearningJourney | null> | FailedResponse
+  >,
 ) {
   res.setHeader("Content-Type", "application/json");
 
@@ -23,33 +25,33 @@ export default async function handler(
     return;
   }
 
-  // Check if "userID" exists
-  try {
-    if ((await isUserExist(userID)) === false) {
-      console.error(new Error(`User with "userID" is not found`));
-      res.status(404).json({ status: "failed", message: "User Not Found" });
-      return;
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ status: "failed", message: "Server Error" });
+  // Check if "learningJourneyID" is a valid UUID
+  const learningJourneyID = (req.query.learningJourneyID ?? "") as string;
+  parseResult = zod.string().uuid().safeParse(learningJourneyID);
+  if (parseResult.success === false) {
+    console.error(
+      new Error(`"learningJourneyID" is not a valid UUID: `, {
+        cause: parseResult.error,
+      }),
+    );
+
+    res
+      .status(404)
+      .json({ status: "failed", message: "Learning Journey Not Found" });
     return;
   }
 
   if (req.method === "GET") {
-    // Check if "learningJourneyID" is a valid UUID
-    const learningJourneyID = (req.query.learningJourneyID ?? "") as string;
-    parseResult = zod.string().uuid().safeParse(learningJourneyID);
-    if (parseResult.success === false) {
-      console.error(
-        new Error(`"learningJourneyID" is not a valid UUID: `, {
-          cause: parseResult.error,
-        }),
-      );
-
-      res
-        .status(404)
-        .json({ status: "failed", message: "Learning Journey Not Found" });
+    // Check if "userID" exists
+    try {
+      if ((await isUserExist(userID)) === false) {
+        console.error(new Error(`User with "userID" is not found`));
+        res.status(404).json({ status: "failed", message: "User Not Found" });
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: "failed", message: "Server Error" });
       return;
     }
 
@@ -85,8 +87,121 @@ export default async function handler(
 
       res.status(500).json({ status: "failed", message: "Server Error" });
     }
+  } else if (req.method === "POST") {
+    const parseResult = zod
+      .object({
+        prerequisiteIDs: zod.array(zod.string().uuid()),
+      })
+      .safeParse(req.body);
+
+    if (parseResult.success === false) {
+      console.error(
+        new Error(`"prerequisiteIDs" are invalid UUIDs: `, {
+          cause: parseResult.error,
+        }),
+      );
+
+      res
+        .status(400)
+        .json({ status: "failed", message: "Invalid JSON Schema" });
+      return;
+    }
+
+    // Check if "userID", "learningJourneyID", and "prerequisiteIDs" exist
+    try {
+      const results = await Promise.all([
+        supabase.from("user").select("id").eq("id", userID).maybeSingle(),
+        supabase
+          .from("learning_journey")
+          .select("id")
+          .eq("id", learningJourneyID)
+          .maybeSingle(),
+        supabase
+          .from("learning_material")
+          .select("id")
+          .in("id", parseResult.data.prerequisiteIDs),
+      ]);
+
+      if (results[0].error !== null) {
+        throw new Error(`Error when get a user based on "userID": `, {
+          cause: results[0].error,
+        });
+      }
+
+      if (results[1].error !== null) {
+        throw new Error(
+          `Error when get a learning journey based on "learningJourneyID": `,
+          { cause: results[1].error },
+        );
+      }
+
+      if (results[2].error !== null) {
+        throw new Error(
+          `Error when get learning materials based on "prerequisiteIDs": `,
+          { cause: results[2].error },
+        );
+      }
+
+      if (results[0].data === null) {
+        console.error(new Error(`User with "userID" is not found`));
+        res.status(404).json({ status: "failed", message: "User Not Found" });
+        return;
+      }
+
+      if (results[1].data === null) {
+        console.error(
+          new Error(`Learning Journey with "learningJourneyID" is not found`),
+        );
+
+        res
+          .status(404)
+          .json({ status: "failed", message: "Learning Journey Not Found" });
+        return;
+      }
+
+      if (
+        results[2].data === null ||
+        results[2].data.length < parseResult.data.prerequisiteIDs.length
+      ) {
+        console.error(
+          new Error(`Learning Materials with "prerequisiteIDs" are not found`),
+        );
+
+        res
+          .status(400)
+          .json({ status: "failed", message: "Invalid JSON Schema" });
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: "failed", message: "Server Error" });
+      return;
+    }
+
+    try {
+      await supabase.from("studied_learning_material").insert(
+        parseResult.data.prerequisiteIDs.map((prerequisiteID) => {
+          return {
+            learning_journey_id: learningJourneyID,
+            learning_material_id: prerequisiteID,
+            is_studied: false,
+          };
+        }),
+      );
+
+      res.status(201).json({ status: "success", data: null });
+    } catch (error) {
+      console.error(
+        new Error(
+          `Error when establish relationship between "learning_journey" and "learning_material" table: `,
+          { cause: error },
+        ),
+      );
+
+      res.status(500).json({ status: "failed", message: "Server Error" });
+    }
   } else {
-    handleInvalidMethod(res, ["GET"]);
+    handleInvalidMethod(res, ["GET", "POST"]);
   }
 }
 
